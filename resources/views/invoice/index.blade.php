8064523fe5ac32569cd7edbb7f35210cda0861f2
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2020-06-06 10:04:28 +0700

    project selesai

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -45,6 +45,11 @@
                                             <form action="{{ route('invoice.destroy', $row->id) }}" method="POST">
                                                 @csrf
                                                 <input type="hidden" name="_method" value="DELETE">
+                                                
+                                                <!-- TAMBAHKAN BARIS CODE INI -->
+                                                <a href="{{ route('invoice.print', $row->id) }}" class="btn btn-secondary btn-sm">Print</a>
+                                                <!-- TAMBAHKAN BARIS CODE INI -->
+                                                
                                                 <a href="{{ route('invoice.edit', $row->id) }}" class="btn btn-warning btn-sm">Edit</a>
                                                 <button class="btn btn-danger btn-sm">Delete</button>
                                             </form>
